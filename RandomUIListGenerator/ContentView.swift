2d6677ee9e1a3992d4b0fb825d351a5f80bacf9b
RaviKiranReddyS <ravikiranreddy.soorasani@gmail.com>
2022-02-20 11:17:52 -0500

    Updated logic to randomly add data to the list view until the max items are reached.

--- RandomUIListGenerator/ContentView.swift
+++ RandomUIListGenerator/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // The goal of this project is to randomly add data to the list and display in the view on click of the button. At this time, no additional check within the list is done.
+    var myData = ["SVR","Padma","RaviKiran","Nishita","Srihitha","Riyansh"]
+    
+    @State var RandomList = [String]()
+    @State var dataCounter = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            List(RandomList, id: \.self){ arrayElement in
+                        Text(arrayElement)
+                    }
+            
+            Spacer()
+            Button("Add Data to List"){
+                let randomNumber = Int.random(in: 0...myData.count-1)
+                        
+                if(dataCounter < myData.count)
+                {
+                    RandomList.append(myData[randomNumber])
+                    dataCounter += 1
+                }
+            }
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
+        
+        
+            
     }
 }
 
