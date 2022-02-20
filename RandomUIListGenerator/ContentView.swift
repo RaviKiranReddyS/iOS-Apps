90cb24c0eb288b3ca957f0cda3e576ad04e89e93
RaviKiranReddyS <ravikiranreddy.soorasani@gmail.com>
2022-02-20 10:36:56 -0500

    Initial Commit

---
+++ RandomUIListGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomUIListGenerator
+//
+//  Created by Ravi Kiran Reddy on 2/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUIListGenerator/RandomUIListGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUIListGeneratorApp.swift
+//  RandomUIListGenerator
+//
+//  Created by Ravi Kiran Reddy on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUIListGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
